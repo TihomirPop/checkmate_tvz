d46cd5a878da1a8acd50a0bc9cba510c106c32fc
TihomirPop <tihomirpopovic42@gmail.com>
2026-02-11 16:04:32 +0100

    extract board to class

---
+++ lib/models/chess_board.dart
@@ -0,0 +1,83 @@
+import 'package:checkmate_tvz/models/chess_piece.dart';
+import 'package:checkmate_tvz/models/chess_position.dart';
+
+class ChessBoard {
+  Map<ChessPosition, ChessPiece> pieces = {};
+  
+  void init() {
+    pieces[const ChessPosition(0, 0)] = ChessPiece(
+      type: PieceType.rook,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 1)] = ChessPiece(
+      type: PieceType.knight,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 2)] = ChessPiece(
+      type: PieceType.bishop,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 3)] = ChessPiece(
+      type: PieceType.queen,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 4)] = ChessPiece(
+      type: PieceType.king,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 5)] = ChessPiece(
+      type: PieceType.bishop,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 6)] = ChessPiece(
+      type: PieceType.knight,
+      color: PieceColor.dark,
+    );
+    pieces[const ChessPosition(0, 7)] = ChessPiece(
+      type: PieceType.rook,
+      color: PieceColor.dark,
+    );
+    for (int col = 0; col < 8; col++) {
+      pieces[ChessPosition(1, col)] = ChessPiece(
+        type: PieceType.pawn,
+        color: PieceColor.dark,
+      );
+      pieces[ChessPosition(6, col)] = ChessPiece(
+        type: PieceType.pawn,
+        color: PieceColor.light,
+      );
+    }
+    pieces[const ChessPosition(7, 0)] = ChessPiece(
+      type: PieceType.rook,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 1)] = ChessPiece(
+      type: PieceType.knight,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 2)] = ChessPiece(
+      type: PieceType.bishop,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 3)] = ChessPiece(
+      type: PieceType.queen,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 4)] = ChessPiece(
+      type: PieceType.king,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 5)] = ChessPiece(
+      type: PieceType.bishop,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 6)] = ChessPiece(
+      type: PieceType.knight,
+      color: PieceColor.light,
+    );
+    pieces[const ChessPosition(7, 7)] = ChessPiece(
+      type: PieceType.rook,
+      color: PieceColor.light,
+    );
+  }
+}
--- lib/widgets/chess_board_widget.dart
+++ lib/widgets/chess_board_widget.dart
@@ -1,3 +1,4 @@
+import 'package:checkmate_tvz/models/chess_board.dart';
 import 'package:flutter/material.dart';
 import '../models/chess_piece.dart';
 import '../models/chess_position.dart';
@@ -11,96 +12,23 @@ class ChessBoardWidget extends StatefulWidget {
 }
 
 class _ChessBoardWidgetState extends State<ChessBoardWidget> {
-  Map<ChessPosition, ChessPiece> pieces = {};
+  ChessBoard chessBoard = ChessBoard();
   ChessPosition? dragSourcePosition;
 
   @override
   void initState() {
     super.initState();
-    pieces[const ChessPosition(0, 0)] = ChessPiece(
-      type: PieceType.rook,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 1)] = ChessPiece(
-      type: PieceType.knight,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 2)] = ChessPiece(
-      type: PieceType.bishop,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 3)] = ChessPiece(
-      type: PieceType.queen,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 4)] = ChessPiece(
-      type: PieceType.king,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 5)] = ChessPiece(
-      type: PieceType.bishop,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 6)] = ChessPiece(
-      type: PieceType.knight,
-      color: PieceColor.dark,
-    );
-    pieces[const ChessPosition(0, 7)] = ChessPiece(
-      type: PieceType.rook,
-      color: PieceColor.dark,
-    );
-    for (int col = 0; col < 8; col++) {
-      pieces[ChessPosition(1, col)] = ChessPiece(
-        type: PieceType.pawn,
-        color: PieceColor.dark,
-      );
-      pieces[ChessPosition(6, col)] = ChessPiece(
-        type: PieceType.pawn,
-        color: PieceColor.light,
-      );
-    }
-    pieces[const ChessPosition(7, 0)] = ChessPiece(
-      type: PieceType.rook,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 1)] = ChessPiece(
-      type: PieceType.knight,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 2)] = ChessPiece(
-      type: PieceType.bishop,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 3)] = ChessPiece(
-      type: PieceType.queen,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 4)] = ChessPiece(
-      type: PieceType.king,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 5)] = ChessPiece(
-      type: PieceType.bishop,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 6)] = ChessPiece(
-      type: PieceType.knight,
-      color: PieceColor.light,
-    );
-    pieces[const ChessPosition(7, 7)] = ChessPiece(
-      type: PieceType.rook,
-      color: PieceColor.light,
-    );
+    chessBoard.init();
   }
 
   void _onPieceDropped(ChessPiece piece, ChessPosition targetPosition) {
     setState(() {
       // Remove piece from source position
       if (dragSourcePosition != null) {
-        pieces.remove(dragSourcePosition);
+        chessBoard.pieces.remove(dragSourcePosition);
       }
       // Add piece to target position
-      pieces[targetPosition] = piece;
+      chessBoard.pieces[targetPosition] = piece;
       // Clear drag source
       dragSourcePosition = null;
     });
@@ -136,7 +64,7 @@ class _ChessBoardWidgetState extends State<ChessBoardWidget> {
             final col = index % 8;
             final isLight = (row + col) % 2 == 0;
             final position = ChessPosition(row, col);
-            final piece = pieces[position];
+            final piece = chessBoard.pieces[position];
 
             return ChessField(
               isLight: isLight,
